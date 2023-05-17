422006f29ce1b1599c7f0f45d1c71394628b1c3f
Robertas <robertasqq@gmail.com>
2023-05-17 17:26:56 +0300

    4 database tables were created or edited

--- backend.py
+++ backend.py
@@ -15,12 +15,13 @@ session = sessionmaker(bind=engine)()
 ## Creating tables
 
 # User table
-class User_(Base):
+class User(Base):
     __tablename__ = "user"
     id = mapped_column(Integer, primary_key=True)
     first_name = mapped_column(String(50), nullable=False)
     last_name = mapped_column(String(50), nullable=False)
-    listings = relationship("Listing_", back_populates="user")
+    user_name = mapped_column(String(50), nullable=False)
+    operation = relationship("Operation", back_populates="user")
 
     ## sito nereikia kai yra Base
     # def __init__(self, **kw: Any):
@@ -29,30 +30,43 @@ class User_(Base):
     #         setattr(self, key, value)
 
     def __repr__(self):
-        return f"({self.id}, {self.first_name}, {self.last_name})"
+        return f"({self.id}, {self.first_name}, {self.last_name}, {self.user_name})"
     
 
-#Listings table
-class Listing_(Base):
-    __tablename__ = "listing_"
+#Operation table
+class Operation(Base):
+    __tablename__ = "operation"
     id = mapped_column(Integer, primary_key=True)
-    amount = mapped_column(String(50), nullable=False)
+    amount = mapped_column(Integer, nullable=False, default=0)
     comment = mapped_column(String(150), nullable=False, default="")
     date_ = mapped_column(DateTime, default=datetime.today().replace(microsecond=0))
     user_id = mapped_column(Integer, ForeignKey("user.id"))
-    user = relationship("User_", back_populates="listings")
-
-
-    ## sito nereikia kai yra Base
-    # def __init__(self, **kw: Any):
-    #     # super().__init__(**kw)
-    #     for key, value in kw.items():
-    #         setattr(self, key, value)
+    type_id = mapped_column(Integer, ForeignKey("type.id"))
+    purpose_id = mapped_column(Integer, ForeignKey("purpose.id"))
+    user = relationship("User", back_populates="operation")
+    type = relationship("Type", back_populates="operation")
+    purpose = relationship("Purpose", back_populates="operation")
 
     def __repr__(self):
         return f"({self.id}, {self.amount}, {self.comment}, {self.date_})"
 
-# , back_populates="listing_" - su situo meto erorus
+
+# Type table
+class Type(Base):
+    __tablename__ = "type"
+    id = mapped_column(Integer, primary_key=True)
+    operation_type = mapped_column(String(20), default="") # earnings or spendings
+    operation = relationship("Operation", back_populates="type")
+    purpose = relationship("Purpose", back_populates="type")
+
+# Purpose table
+class Purpose(Base):
+    __tablename__ = "purpose"
+    id = mapped_column(Integer, primary_key=True)
+    operation_purpose = mapped_column(String(50), default="")
+    type_id = mapped_column(Integer, ForeignKey("type.id"))
+    type = relationship("Type", back_populates="purpose")
+    operation = relationship("Operation", back_populates="purpose")
 
 
 Base.metadata.create_all(engine)
@@ -62,7 +76,7 @@ Base.metadata.create_all(engine)
 ###### Actions with db tables
 
 def query_user(session):
-    users = session.query(User_).all()
+    users = session.query(User).all()
     users_list = [
                 [item.id, item.first_name, item.last_name]
                 for item in users
@@ -70,20 +84,20 @@ def query_user(session):
     return users_list
 
 def create_user(f_name, l_name):
-    customer = User_(first_name=f_name, last_name=l_name)
+    customer = User(first_name=f_name, last_name=l_name)
     session.add(customer)
     session.commit()
 
 def delete_user(session, delete_id):
     try:
-        user_on_delete = session.get(User_, int(delete_id))
+        user_on_delete = session.get(User, int(delete_id))
         session.delete(user_on_delete)
         session.commit()
     except Exception as e:
         print(f"Error: {e}")
 
 def select_from_user_list_table(session, row_value):
-    items = session.query(User_).all()
+    items = session.query(User).all()
     items_list = [
                 [item.id, item.first_name, item.last_name]
                 for item in items
@@ -91,7 +105,7 @@ def select_from_user_list_table(session, row_value):
     return items_list[row_value]
 
 def select_from_user_listings_table(session, row_value, user_id_n):
-    items = session.query(Listing_).filter_by(user_id=user_id_n)
+    items = session.query(Operation).filter_by(user_id=user_id_n)
     items_list = [
                 [item.id, item.amount, item.comment, item.date_]
                 for item in items
@@ -99,12 +113,12 @@ def select_from_user_listings_table(session, row_value, user_id_n):
     return items_list[row_value]
 
 def insert_new_record(amount_n, comment_n, user_id_n):
-    items = Listing_(amount=amount_n, comment=comment_n, user_id=user_id_n)
+    items = Operation(amount=amount_n, comment=comment_n, user_id=user_id_n)
     session.add(items)
     session.commit()
 
 def query_user_items(session, user_id_n):
-    items = session.query(Listing_).filter_by(user_id=user_id_n)
+    items = session.query(Operation).filter_by(user_id=user_id_n)
     items_list = [
                 [item.id, item.amount, item.comment, item.date_]
                 for item in items
@@ -112,7 +126,7 @@ def query_user_items(session, user_id_n):
     return items_list
 
 def query_all_items(session):
-    items = session.query(Listing_).join(User_).all()
+    items = session.query(Operation).join(User).all()
     items_list = [
                 [item.id, item.user.first_name, item.amount, item.comment, item.date_]
                 for item in items
